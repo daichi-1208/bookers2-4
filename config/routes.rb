050aca80e1365b5d7bf89bf30fe962b74d1c67b5
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-01-28 17:12:25 +0900

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+  get 'relationships/destroy'
   root 'home#top'
   get 'home/about' => "home#about"
 
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :users,only: [:show,:index,:edit,:update] do
+  	resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_232832) do
+ActiveRecord::Schema.define(version: 2020_01_28_062255) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
